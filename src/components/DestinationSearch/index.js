0a4ac60e5e04a096385fb21d4bff3cda0121a856
swathikolanpaka <swathie72@gmail.com>
2023-07-27 08:18:48 +0000

    solution

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,17 +1,47 @@
 // Write your code
 import {Component} from 'react'
+
 import DestinationItem from '../DestinationItem'
+
 import './index.css'
 
 class DestinationSearch extends Component {
+  state = {searchInput: '', finalDestinationList: ''}
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  render() {
+    const {searchInput, finalDestinationList} = this.state
 
     const {destinationsList} = this.props
 
-  state = {searchInput: '', finalDestinationList: destinationsList}
+    const searchResult = destinationsList.filter(eachPlace =>
+      eachPlace.name.includes(searchInput),
+    )
 
-  render() {
+    return (
+      <div className="bg-container">
+        <h1>Destination Search</h1>
+        <input
+          type="search"
+          onChange={this.onChangeSearchInput}
+          value={searchInput}
+        />
 
-    return <h1>Destination Search</h1>
+        <ul>
+          {searchResult.map(eachPlace => (
+            <DestinationItem
+              destinationList={eachPlace.destinationList}
+              key={eachPlace.id}
+            />
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
 
